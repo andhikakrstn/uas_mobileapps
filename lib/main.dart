9b9db34052025bc1c3c44deaac3bb3e18b97a5dc
andhikakrstn <72987108+andhikakrstn@users.noreply.github.com>
2021-07-01 00:10:52 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'widgets/DataTableDemo.dart';
+
+void main() {
+  runApp(
+    HomeApp(),
+  );
+}
+
+class HomeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: DataTableDemo(),
+    );
+  }
+}
